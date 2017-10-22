a5cd33e9483ec2e45fed6cf94be47fd17363763e
武书生 <double_dongli@foxmail.com>
2017-10-22 17:58:55 +0800

    Create fitline2.py

---
+++ code/stocfitline/fitline2.py
@@ -0,0 +1,5 @@
+from numpy import *
+import logRegres
+dataArr,labelMat = logRegres.loadDataSet()
+weights = logRegres.stocGradAscent0(array(dataArr),labelMat)
+logRegres.plotBestFit(matrix(weights).transpose())
